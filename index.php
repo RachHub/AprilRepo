241552359e00261a8e8b68232aea4835cd6d6bc5
RachHub <racheldonnelly7@gmail.com>
2018-04-08 14:18:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
 echo "Hello world from Heroku server";
-echo "<p>Hello World from Heroku</p>";
+
+/* Filename: jsonapi.php */    
+/* Declare your variables */      
+$items = array();    
+/* Populate your array with a list of dummy strings */      
+for($i = 0; $i < 10; $i++) {         
+    $items[$i] = "This is message $i";        
+}    
+/* Set the header type for output */        
+header('Content-Type: application/json');    
+/* Create a variable to hold your JSON data */  
+ $jsonOutput =  json_encode($items);    
+/* Output the JSON data */       
+ echo $jsonOutput; 
+?>
 
